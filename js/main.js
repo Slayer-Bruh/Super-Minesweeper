128953e39494fe1f48d067f716469e8eff7a907d
Slayer-Bruh <nevo432100@gmail.com>
2026-06-13 19:00:59 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -19,20 +19,22 @@ const gLevels = [
     MINES: 32,
   },
 ]
-const gLevel = gLevels[1]
+const gLevel = gLevels[0]
 
 const gGame = {
-  isOn: false,
   revealedCount: 0,
   markedCount: 0,
   secsPassed: 0,
   lives: 3,
+  status: 'starting',
+  correctlyMarked: 0,
 }
 
 function onInit() {
   gBoard = buildBoard()
   renderBoard(gBoard)
   renderLives(gGame)
+  renderSmiley()
   console.table(gBoard)
 }
 
@@ -95,37 +97,55 @@ function setMinesNegsCount(board) {
 }
 
 function onCellClicked(elCell, i, j) {
-  if (!gGame.isOn) {
-    setRandomMines(gBoard, gLevel, { i: i, j: j })
-    setMinesNegsCount(gBoard)
-    gGame.isOn = true
+  if (gGame.status === 'win' || gGame.status === 'loss') return
+  if (gGame.status === 'starting') {
+    startGame(i, j)
+    expandReveal(gBoard, i, j)
+    renderBoard(gBoard)
+    return
   }
-  var currCell = gBoard[i][j]
-  if (currCell.isRevealed) return
 
-  currCell.isRevealed = true
+  var currCell = gBoard[i][j]
+  if (currCell.isRevealed || currCell.isMarked) return
+
+  expandReveal(gBoard, i, j)
 
   if (currCell.isMine) {
     gGame.lives--
     renderLives(gGame)
 
     if (gGame.lives === 0) {
-      console.log('Game Over here')
+      gGame.status = 'loss'
     } else {
       setTimeout(() => {
-        currCell.isRevealed = false
+        hideCell(i, j)
         renderBoard(gBoard)
       }, 1500)
     }
   }
   renderBoard(gBoard)
+  checkGameOver()
 }
 
 function onCellMarked(ev, i, j) {
   ev.preventDefault()
+
+  if (gGame.status === 'win' || gGame.status === 'loss') return
   var currCell = gBoard[i][j]
+  if (currCell.isRevealed) return
+
   currCell.isMarked = !currCell.isMarked
+
+  if (currCell.isMarked) {
+    gGame.markedCount++
+    if (currCell.isMine) gGame.correctlyMarked++
+  } else {
+    gGame.markedCount--
+    if (currCell.isMine) gGame.correctlyMarked--
+  }
+
   renderBoard(gBoard)
+  checkGameOver()
 }
 
 function setRandomMines(board, level, safeCell) {
@@ -141,20 +161,93 @@ function setRandomMines(board, level, safeCell) {
 }
 
 function restartGame() {
-  gGame.isOn = false
   gGame.revealedCount = 0
   gGame.markedCount = 0
   gGame.secsPassed = 0
   gGame.lives = 3
+  gGame.status = 'starting'
+  gGame.correctlyMarked = 0
 
   onInit()
 }
 
 function renderLives(game) {
   var elLives = document.querySelector('.lives')
-  elLives.innerText = game.lives === 0 ? 'Game Over' : '❤️'.repeat(game.lives)
+  elLives.innerText = '❤️'.repeat(game.lives)
 }
 
-function checkGameOver() {}
+function checkGameOver() {
+  var totalSafeCells = gLevel.SIZE * gLevel.SIZE - gLevel.MINES
+  const elLives = document.querySelector('.lives')
 
-function expandReveal(board, elCell, i, j) {}
+  if (
+    gGame.correctlyMarked === gLevel.MINES &&
+    gGame.revealedCount === totalSafeCells
+  ) {
+    gGame.status = 'win'
+    elLives.innerText = 'You Won!'
+    elLives.style.color = '#67B279'
+  }
+
+  if (gGame.lives === 0) {
+    gGame.status = 'loss'
+    elLives.innerText = 'You Lose!'
+    elLives.style.color = '#d41313'
+  }
+  renderSmiley()
+}
+
+function revealCell(i, j) {
+  var currCell = gBoard[i][j]
+  currCell.isRevealed = true
+  gGame.revealedCount++
+}
+
+function hideCell(i, j) {
+  var currCell = gBoard[i][j]
+  if (!currCell.isRevealed) return
+  currCell.isRevealed = false
+  gGame.revealedCount--
+}
+
+function startGame(i, j) {
+  setRandomMines(gBoard, gLevel, { i: i, j: j })
+  setMinesNegsCount(gBoard)
+  gGame.status = 'running'
+}
+
+function expandReveal(board, i, j) {
+  var currCell = board[i][j]
+
+  if (currCell.isRevealed || currCell.isMarked || currCell.isMine) return
+
+  revealCell(i, j)
+
+  if (currCell.minesAroundCount > 0) return
+
+  for (var negI = i - 1; negI <= i + 1; negI++) {
+    if (negI < 0 || negI >= gLevel.SIZE) continue
+
+    for (var negJ = j - 1; negJ <= j + 1; negJ++) {
+      if (negJ < 0 || negJ >= gLevel.SIZE) continue
+      if (negI === i && negJ === j) continue
+
+      expandReveal(board, negI, negJ)
+    }
+  }
+}
+
+function renderSmiley() {
+  const elSmiley = document.querySelector('.smiley')
+
+  switch (gGame.status) {
+    case 'win':
+      elSmiley.innerText = '😎'
+      break
+    case 'loss':
+      elSmiley.innerText = '😭'
+      break
+    default:
+      elSmiley.innerText = '🙂'
+  }
+}
